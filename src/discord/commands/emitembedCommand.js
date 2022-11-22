49e72227e7e8213131f848e4b2881d74a03ae7d9
AxleTiger <bloodymountain@outlook.com>
2022-11-21 21:51:13 -0500

    Your token was found here:

---
+++ src/discord/commands/emitembedCommand.js
@@ -0,0 +1,61 @@
+const { EmbedBuilder, ActionRowBuilder, SelectMenuBuilder, ButtonBuilder, ButtonStyle } = require('discord.js')
+const config = require('../../../config.json')
+
+module.exports = {
+    name: 'emitembed',
+    description: 'Emits and embed',
+    options: [
+        {
+            name: 'title',
+            description: 'embed title (required)',
+            type: 3,
+            required: true
+        },
+        {
+            name: 'context',
+            description: 'the message that will be sent',
+            type: 3,
+            required: true
+        },
+        {
+            name: 'footer',
+            description: 'footer text',
+            type: 3,
+            required: false
+        }
+        
+      ],
+  
+    execute: async (interaction, client, InteractionCreate) => {
+
+        const title = interaction.options.getString("title")
+        let context = interaction.options.getString('context').replaceAll('\\n', '\n')
+        const footer = interaction.options.getString("footer") || "hyperbolic"
+       
+        const chat = {
+            title: `${title}`,
+            description: (`${context}`),
+            timestamp: new Date().toISOString(),
+            footer: {
+                text: `${footer}`,
+            },
+        };
+
+        
+
+     if ((await interaction.guild.members.fetch(interaction.user)).roles.cache.has(config.discord.managementRole)) {
+        await interaction.channel.send({embeds: [chat] });
+        await interaction.reply({ content: 'This is a reply message confirming your command was sent, please delete me!', ephemeral: true });
+        const emitlog = {
+            title: `Embedded Message Emit Logs`,
+            description: (`Title:\`${title}\`\nContext:\`${context}\`Footer:\`${footer}\`\n\nwas sent by:\n**Username**: ${interaction.user}\n**ID**: ${interaction.user.id}`),
+            timestamp: new Date().toISOString(),
+            footer: {text: `Hyperbolic Logging`,},
+        };
+        await client.channels.cache.get(config.channels.loggingChannel).send({embeds: [emitlog] })
+
+    } else {
+        await interaction.reply({ content: 'You do not have permission to run this command.', ephemeral: true })
+        }
+    }
+};;
---
+++ src/discord/commands/pingCommand.js
@@ -0,0 +1,16 @@
+const { EmbedBuilder } = require("discord.js")
+
+module.exports = {
+  name: "ping",
+  description: "Shows the latency of the bot.",
+
+  execute: async (interaction, client) => {
+    const embed = new EmbedBuilder()
+      .setColor(0x0099FF)
+      .setTitle("🏓 Pong!")
+      .setDescription(`Latency: ${client.ws.ping}ms`)
+      .setFooter({ text: `made by /credits  | /help [command] for more information`, iconURL: 'https://i.imgur.com/FeOykcL.png' })
+      
+    interaction.reply({ embeds: [embed] })
+  },
+};
