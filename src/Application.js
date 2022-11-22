f0e27c3dd503e95bddf19d0ff14474ef212ca07b
AxleTiger <bloodymountain@outlook.com>
2022-11-21 21:50:33 -0500

    g

---
+++ src/Application.js
@@ -0,0 +1,15 @@
+
+const DiscordManager = require("./discord/DiscordManager")
+
+class Application {
+  async register() {
+    this.discord = new DiscordManager(this);
+
+  }
+
+  async connect() {
+    this.discord.connect();
+  }
+}
+
+module.exports = new Application();
